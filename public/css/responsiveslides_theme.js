6f4c5daaa781bcb05e2012ea0f02eba7a01dfd2d
shengshampoo <shengzh@tutanota.com>
2016-09-07 00:12:20 +0800

    Create responsiveslides_theme.js

---
+++ public/css/responsiveslides_theme.js
@@ -0,0 +1,11 @@
+    $(function () {
+      $("#slides1").responsiveSlides({
+        auto: true,
+        pager: true,
+        nav: true,
+        speed: 500,
+        timeout: 4000, 
+        maxwidth: 800,
+        namespace: "transparent-btns"
+      });
+    });
