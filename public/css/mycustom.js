7cbbdd60ae494da30f7df18d22282c345ce43603
shengshampoo <shengzh@tutanota.com>
2016-09-10 00:22:55 +0800

    Create mycustom.js

---
+++ public/css/mycustom.js
@@ -0,0 +1,9 @@
+/* loading lightGallery.io css*/
+$(document).ready(function() {
+    $('#lightgallery').lightGallery({
+    thumbnail:true,
+    fullScreen:true,
+    autoplay:true,
+    pause:8000,
+      }); 
+    });
