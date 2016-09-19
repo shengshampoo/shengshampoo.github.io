8488c7fc2a7a2b1febbfa65cd7adfd95cd173114
shengshampoo <shengzh@tutanota.com>
2016-09-19 17:49:14 +0800

    Update mycustom.js

--- public/css/mycustom.js
+++ public/css/mycustom.js
@@ -38,22 +38,3 @@ $(document).ready(function() {
     }
     });
     });
-  
-$(document).ready(function() {
-  /* https://davemcmillan.net/use-jquery-to-display-an-image-alt-tag-as-a-caption-overlay*/
-  /* display image caption on top of image */
-    $('.owl-carousel').each(function() {
-          var imageCaption = $(this).attr("alt");
-		  
-          if (imageCaption != '') {
-              var imgWidth = $(this).width();
-              var imgHeight = $(this).height();
-              var position = $(this).position();
-              var positionTop = (position.top + imgHeight - 26);
-              $("<span class='img-caption'><em>"+imageCaption+"</em></span>")
-              .css({"position":"absolute", "top":positionTop+"px", "left":"0", "width":imgWidth +"px"})
-              .insertAfter(this);
-          }
-		  
-     });	
-  });
