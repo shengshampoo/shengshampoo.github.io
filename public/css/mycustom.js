0d022b53c87c2de3454ec36b8705769481c50c26
shengshampoo <shengzh@tutanota.com>
2016-09-19 17:40:24 +0800

    Update mycustom.js

--- public/css/mycustom.js
+++ public/css/mycustom.js
@@ -38,23 +38,22 @@ $(document).ready(function() {
     }
     });
     });
-
+  
 $(document).ready(function() {
-	 
-// display image caption on top of image
-// https://davemcmillan.net/use-jquery-to-display-an-image-alt-tag-as-a-caption-overlay/
-$("#content img").each(function() {
-    var imageCaption = $(this).attr("alt");
+  // https://davemcmillan.net/use-jquery-to-display-an-image-alt-tag-as-a-caption-overlay/	  
+  // display image caption on top of image
+    $("#content img").each(function() {
+          var imageCaption = $(this).attr("alt");
 		  
-    if (imageCaption != '') {
-        var imgWidth = $(this).width();
-        var imgHeight = $(this).height();
-        var position = $(this).position();
-        var positionTop = (position.top + imgHeight - 26);
-        $("<span class='img-caption'><em>"+imageCaption+"</em></span>")
-        .css({"position":"absolute", "top":positionTop+"px", "left":"0", "width":imgWidth +"px"})
-        .insertAfter(this);
-        }
-	  
+          if (imageCaption != '') {
+              var imgWidth = $(this).width();
+              var imgHeight = $(this).height();
+              var position = $(this).position();
+              var positionTop = (position.top + imgHeight - 26);
+              $("<span class='img-caption'><em>"+imageCaption+"</em></span>")
+              .css({"position":"absolute", "top":positionTop+"px", "left":"0", "width":imgWidth +"px"})
+              .insertAfter(this);
+          }
+		  
      });	
-});
+  });
