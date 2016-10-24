4ae7390f57732c0729ac9785c1be940293108e21
shengshampoo <shengzh@tutanota.com>
2016-10-25 03:37:08 +0800

    Update mycustom.js

--- public/css/mycustom.js
+++ public/css/mycustom.js
@@ -38,3 +38,14 @@ $(document).ready(function() {
     }
     });
     });
+
+/* loading Slippry js*/
+jQuery('#slippry-query').slippry({
+  // general elements & wrapper
+  slippryWrapper: '<div class="sy-box jquery-demo" />', // wrapper to wrap everything, including pager
+  // options
+  adaptiveHeight: false, // height of the sliders adapts to current slide
+  useCSS: false, // true, false -> fallback to js if no browser support
+  autoHover: false,
+  transition: 'horizontal'
+});
