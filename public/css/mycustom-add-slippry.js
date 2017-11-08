0e5a9e127806aabdd047c33adf78cd73dbea55a6
shengshampoo <shengzh@tutanota.com>
2017-11-08 17:33:31 +0800

    Create mycustom-add-slippry.js

---
+++ public/css/mycustom-add-slippry.js
@@ -0,0 +1,24 @@
+/* loading Slippry js addition*/
+jQuery(document).ready(function(){
+    jQuery('#slippry-query2').slippry({
+    // general elements & wrapper
+    slippryWrapper: '<div class="sy-box jquery-demo" />', // wrapper to wrap everything, including pager
+    // options
+    adaptiveHeight: false, // height of the sliders adapts to current slide
+    useCSS: false, // true, false -> fallback to js if no browser support
+    autoHover: false,
+    transition: 'horizontal'
+    });
+    });    
+/* loading Slippry js*/
+jQuery(document).ready(function(){
+    jQuery('#slippry-query3').slippry({
+    // general elements & wrapper
+    slippryWrapper: '<div class="sy-box jquery-demo" />', // wrapper to wrap everything, including pager
+    // options
+    adaptiveHeight: false, // height of the sliders adapts to current slide
+    useCSS: false, // true, false -> fallback to js if no browser support
+    autoHover: false,
+    transition: 'horizontal'
+    });
+    });    
